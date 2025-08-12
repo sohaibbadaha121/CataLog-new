e324f1bec4539ea3d9eca8d07abb715d94109b18
Sohaib Badaha <sohaibbadaha@192.168.1.12>
2025-08-12 18:45:28 +0300

    small change to trigger PR

---
+++ src/script.js
@@ -0,0 +1,68 @@
+const catscontainer = document.getElementById("cats");
+const prevbtn = document.getElementById("prev-btn");
+const nextbtn = document.getElementById("next-btn");
+let skip = 0;
+const limit = 8;
+let catlist = [];
+
+fetchcats();
+async function fetchcats() {
+  catscontainer.innerHTML = "<p>Loading ... </p>";
+  try {
+    const req = await fetch(
+      `https://cataas.com/api/cats?skip=${skip}&limit=${limit}`
+    );
+
+    const cats = await req.json();
+    console.log(cats);
+    catlist = [...cats];
+    updatefavorit();
+  } catch (error) {
+    catscontainer.innerHTML("error ");
+  }
+}
+
+function updatefavorit() {
+  catscontainer.innerHTML = "";
+  catlist.forEach((cat) => {
+    const card = document.createElement("div");
+    card.classList.add("catimg");
+    card.innerHTML = `
+        <img src="https://cataas.com/cat/${cat.id}" alt="cat">
+        <button class="fav-btn" onclick="addfavorites('${cat.id}')">
+         ${isfavorite(cat.id) ? "❤️" : "🤍"}
+         </button>
+        `;
+    catscontainer.appendChild(card);
+  });
+}
+
+nextbtn.addEventListener("click", () => {
+  skip += limit;
+  fetchcats();
+});
+
+prevbtn.addEventListener("click", () => {
+  if (skip >= limit) {
+    skip -= limit;
+    fetchcats();
+  }
+});
+
+function addfavorites(catId) {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  if (favorites.includes(catId)) {
+    favorites = favorites.filter((id) => id !== catId);
+  } else {
+    favorites.push(catId);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  updatefavorit();
+}
+
+function isfavorite(catId) {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  return favorites.includes(catId);
+}
